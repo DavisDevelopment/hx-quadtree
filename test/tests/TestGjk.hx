3d4142f1cb3a46e9d7d73fba3e15c571d8cffa10
Grigoris Mantaos <grigoris.mantaos@siemens.com>
2019-10-22 10:20:55 +0200

    Complete GJK implementation

--- test/tests/TestGjk.hx
+++ test/tests/TestGjk.hx
@@ -1,12 +1,30 @@
 package tests;
 
+import utest.Assert;
+import quadtree.gjk.Gjk;
 import utest.ITest;
+import tests.models.Triangle;
 
 
 class TestGjk implements ITest
 {
+    var gjk: Gjk;
+
+
     public function new() { }
 
 
-    
+    public function setupClass()
+    {
+        gjk = new Gjk();
+    }
+
+
+    function testTriangles()
+    {
+        var t1: Triangle = new Triangle([0, 0], [0, 1], [1, 0]);
+        var t2: Triangle = new Triangle([0, 0], [1, 1], [1, 0]);
+
+        Assert.isTrue(gjk.checkOverlap(t1, t2));
+    }
 }
