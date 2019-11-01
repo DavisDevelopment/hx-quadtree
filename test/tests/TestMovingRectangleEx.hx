4c82100da29e67ba52f4c4989fcf8ee88732d40b
Grigoris Mantaos <grigoris.mantaos@siemens.com>
2019-11-01 16:29:41 +0100

    Slightly improve test coverage

--- test/tests/TestMovingRectangleEx.hx
+++ test/tests/TestMovingRectangleEx.hx
@@ -1,5 +1,6 @@
 package tests;
 
+import tests.models.Point;
 import utest.Assert;
 import tests.models.MovingBox;
 import utest.ITest;
@@ -21,5 +22,14 @@ class TestMovingRectangleEx implements ITest
         Assert.equals(10, movingBox.hullY());
         Assert.equals(110, movingBox.hullWidth());
         Assert.equals(120, movingBox.hullHeight());
+    }
+
+
+    function testIntersectsPoint()
+    {
+        var b: MovingBox = new MovingBox(0, 0, 100, 100, 45);
+        var p: Point = new Point(50, -3);
+
+        Assert.isTrue( MovingRectangleEx.intersectsWithPoint(b.hullX(), b.hullY(), b.hullWidth(), b.hullHeight(), b.angle, p.x, p.y) );
     }
 }
